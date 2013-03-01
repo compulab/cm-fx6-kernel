7c5e6b122f21cd73c9e5a3639e537fc69c9fc072
Igor Grinberg <grinberg@compulab.co.il>
2013-03-01 23:36:27 +0200

    mxc: lcd: support DataImage LCD
    Support DataImage LCD on mxc.
    
    Notice:
    - the .pixclock value is given in picoseconds, not in KHz
    - the FB_SYNC_CLK_LAT_FALL flag is not applicable for the SCF0403 LCD
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>

--- drivers/video/mxc/mxc_lcdif.c
+++ drivers/video/mxc/mxc_lcdif.c
@@ -53,6 +53,20 @@ static struct fb_videomode lcdif_modedb[] = {
 		.vsync_len	= 3,
 		.vmode		= FB_VMODE_NONINTERLACED,
 	},
+	{
+	/* DataImage: 480x800 @ 60 Hz , pixel clk @ 29MHz */
+		.name		= "SCF04-WVGA",
+		.xres		= 480,
+		.yres		= 800,
+		.pixclock	= 34722,
+		.left_margin	= 12,
+		.right_margin	= 16,
+		.upper_margin	= 30,
+		.lower_margin	= 40,
+		.hsync_len	= 26,
+		.vsync_len	= 30,
+		.vmode		= FB_VMODE_NONINTERLACED,
+	},
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
