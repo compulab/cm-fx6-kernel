utilite-computer/linux-kernel-3.0

ARM: i.MX6: cm-fx6: clean up cm_fx6_init_v4l()
Clean up cm_fx6_init_v4l() function:
* declare variables in the beginning of the function
* do not check the return value of memblock_alloc_base() which does not
  return in case of error (panic)
* check return value of memblock_remove() and propagate the error
* IS_ERR() is not suitable as return value, use PRT_ERR() instead
* check return value of dma_declare_coherent_memory() function

Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

arch/arm/mach-mx6/board-cm-fx6.c
@@ -1843,29 +1843,41 @@ static int __init cm_fx6_init_v4l(void)
 	struct platform_device *voutdev;
 	resource_size_t res_mbase = 0;
 	resource_size_t res_msize = cm_fx6_v4l_msize;
+	phys_addr_t phys;
+	int err = 0;
 
 	if (res_msize) {
-		phys_addr_t phys = memblock_alloc_base(res_msize, SZ_4K, SZ_1G);
-		if (!phys) {
-			pr_err("%s: memblock_alloc_base failed: %lx\n",
-				   __func__, (unsigned long) res_msize);
-			return 0;
+		/* memblock_alloc_base() will not return on failure (panic). */
+		phys = memblock_alloc_base(res_msize, SZ_4K, SZ_1G);
+		err = memblock_remove(phys, res_msize);
+		if (err) {
+			pr_err("%s: memblock_remove for base=%lx, size=%lx failed: %d\n",
+			       __func__, (unsigned long) phys,
+			       (unsigned long) res_msize, err);
+			return err;
 		}
-		memblock_remove(phys, res_msize);
+
 		res_mbase = phys;
 	}
 
 	voutdev = imx6q_add_v4l2_output(0);
 	if (IS_ERR(voutdev)) {
-		pr_err("%s: imx6q_add_v4l2_output failed: errno %ld\n",
-			   __func__, IS_ERR(voutdev));
-		return 0;
+		pr_err("%s: imx6q_add_v4l2_output failed: %ld\n",
+			   __func__, PTR_ERR(voutdev));
+		return PTR_ERR(voutdev);
 	}
+
 	if (res_msize && voutdev) {
-		dma_declare_coherent_memory(&voutdev->dev,
-				res_mbase, res_mbase,res_msize,
-				DMA_MEMORY_MAP | DMA_MEMORY_EXCLUSIVE);
+		/* dma_declare_coherent_memory returns 0 on any error */
+		err = dma_declare_coherent_memory(&voutdev->dev,
+					res_mbase, res_mbase,res_msize,
+					DMA_MEMORY_MAP | DMA_MEMORY_EXCLUSIVE);
+		if (err == 0) {
+			platform_device_unregister(voutdev);
+			return -ENOMEM;
+		}
 	}
+
 	return 0;
 }
 

compulab/cm-fx6-kernel

Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>

arch/arm/mach-mx6/board-cm-fx6.c
@@ -1685,29 +1685,39 @@ static int __init cm_fx6_init_v4l(void)
 	phys_addr_t phys;
 	resource_size_t res_mbase = 0;
 	resource_size_t res_msize = cm_fx6_v4l_msize;
+	int err = 0;
 
 	if (res_msize) {
 		pr_info("%s: allocate: %u bytes \n", __func__, res_msize);
+		/* memblock_alloc_base() will not return on failure (panic). */
 		phys = memblock_alloc_base(res_msize, SZ_4K, SZ_1G);
-		if (!phys) {
-			pr_err("%s: memblock_alloc_base() failed \n", __func__);
-			return 0;
+		err = memblock_remove(phys, res_msize);
+		if (err) {
+			pr_err("%s: memblock_remove for base=%lx, size=%lx failed: %d\n",
+			       __func__, (unsigned long) phys,
+			       (unsigned long) res_msize, err);
+			return err;
 		}
-		memblock_remove(phys, res_msize);
+
 		res_mbase = phys;
 	}
 
 	voutdev = imx6q_add_v4l2_output(0);
 	if (IS_ERR(voutdev)) {
 		pr_err("%s: imx6q_add_v4l2_output() failed: %ld \n",
-			   __func__, IS_ERR(voutdev));
-		return 0;
+		       __func__, IS_ERR(voutdev));
+		return PTR_ERR(voutdev);
 	}
 
 	if (res_msize && voutdev) {
-		dma_declare_coherent_memory(&voutdev->dev,
-				res_mbase, res_mbase,res_msize,
-				DMA_MEMORY_MAP | DMA_MEMORY_EXCLUSIVE);
+		/* dma_declare_coherent_memory returns 0 on any error */
+		err = dma_declare_coherent_memory(&voutdev->dev,
+					res_mbase, res_mbase,res_msize,
+					DMA_MEMORY_MAP | DMA_MEMORY_EXCLUSIVE);
+		if (err == 0) {
+			platform_device_unregister(voutdev);
+			return -ENOMEM;
+		}
 	}
 
 	return 0;